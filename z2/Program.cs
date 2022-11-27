e881d0cd2d614931df359fc26bb04d9650999f22
Evgeniy <estrin.j@yandex.ru>
2022-11-27 12:20:32 +0300

    2

---
+++ z2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и 
+//выдаёт сумму цифр в числе.
+//452 -> 11 82 -> 10 9012 -> 12
+
+Console.WriteLine("Введите число");
+var a = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+while ( a >= 1)
+{
+    summ = summ + a % 10;
+    a = a / 10;
+}
+Console.WriteLine("Write the summ > " + summ);
